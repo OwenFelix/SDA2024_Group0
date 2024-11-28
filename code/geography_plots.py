a47a76f613684ddb68e4e8cd4b12db5b4b365042
mabeltje <mabeltraube@gmail.com>
2024-11-28 15:50:16 +0100

    Updated the state borders with a more precise outline

--- code/geography_plots.py
+++ code/geography_plots.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import geopandas as gpd
   
-
-states = gpd.read_file('../tl_2024_us_state/tl_2024_us_state.shp')
+states = gpd.read_file('../state_borders/cb_2018_us_state_500k.shp')
 states = states.to_crs("EPSG:3395")
 
 non_continental = ['HI','VI','MP','GU','AK','AS','PR']
 us49 = states
-for n in non_continental:
-    us49 = us49[us49.STUSPS != n]
+# for n in non_continental:
+#     us49 = us49[us49.STUSPS != n]
 
 voting_results = pd.read_csv('../data/election_results/voting.csv')
 
@@ -22,8 +22,13 @@ democrat_color = '#0000FF'
 
 us49['COLOR'] = np.where(us49['NAME'].isin(trump_states), republican_color, democrat_color)
 
-us49.plot(color = us49['COLOR'], linewidth = 0.5, edgecolor = 'black', legend = True)
+republican_patch = mpatches.Patch(color=republican_color, label='Trump-Winning States')
+democrat_patch = mpatches.Patch(color=democrat_color, label='Biden-Winning States')
 
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+us49.plot(color = us49['COLOR'], linewidth = 0.5, edgecolor = 'black', legend = True, ax=ax)
+
+plt.legend(handles=[republican_patch, democrat_patch], loc='center left', bbox_to_anchor=(0.7, -0.1))
 plt.axis('off')
 plt.title('2020 Presidential Election Results by State')
 
