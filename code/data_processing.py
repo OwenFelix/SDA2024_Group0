de2c2e8c3ea259a2e8372dc0336a2abefa67b752
OwenFelix <owenpoort@hotmail.nl>
2024-12-06 15:29:13 +0100

    removed city data, changed punctuation and added comments

--- code/data_processing.py
+++ code/data_processing.py
@@ -4,7 +4,6 @@ TODO : FILL WITH DESCRIPTION OF CONTENT OF FILE
 """
 
 import pandas as pd  # For data manipulation
-import numpy as np  # For numerical operations
 import matplotlib.pyplot as plt  # For plotting
 import re  # For regular expressions
 import emoji  # For handling emojis
@@ -12,9 +11,9 @@ import string  # For handling string operations
 from nltk.corpus import stopwords  # For stopwords
 from nltk.tokenize import TweetTokenizer  # For tokenizing tweets
 from nltk.stem import WordNetLemmatizer  # For lemmatizing words
-from wordcloud import WordCloud  # For creating wordclouds
 from textblob import TextBlob  # For sentiment analysis
-from langdetect import detect, DetectorFactory  # For detecting the language of a text
+# For detecting the language of a text
+from langdetect import detect, DetectorFactory
 
 DetectorFactory.seed = 0  # For reproducibility
 
@@ -113,7 +112,6 @@ def read_data(file_path):
     """
     Read the data from the given file path
     """
-
     data = pd.read_csv(file_path, chunksize=1000, lineterminator='\n')
     chunk_list = []
     for chunk in data:
@@ -122,18 +120,17 @@ def read_data(file_path):
 
 
 def remove_stopwords(text):
+    """
+    Remove stopwords from the text except negation words
+    """
     stop_words = set(stopwords.words('english'))
     stop_words -= {'not', 'no'}
     return " ".join(word for word in text.split() if word not in stop_words)
 
 
-def gen_z_slang(text):
-    return " ".join(CHAT_WORDS.get(w, w) for w in text.split())
-
-
 def clean_tweet_data(text):
     # GenZ slang treatment
-    text = gen_z_slang(text)
+    text = " ".join(CHAT_WORDS.get(w, w) for w in text.split())
 
     # Remove HTML tags
     text = re.sub(r'<.*?>', '', text)
@@ -141,15 +138,16 @@ def clean_tweet_data(text):
     # Remove URLs entirely
     text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
 
+    # Replace emoji with text for the expression of the emoji
+    text = emoji.demojize(text)
+
     # remove punctuation, but keep !, ?, # and @
-    text = text.translate(str.maketrans('', '', string.punctuation.replace(
-        '!', '').replace('?', '').replace('#', '').replace('@', '')))
+    punctuation_to_replace = ''.join(
+        c for c in string.punctuation if c not in "!?#@")
+    text = re.sub(f"[{re.escape(punctuation_to_replace)}]", " ", text)
 
     # remove the stopwords
     text = remove_stopwords(text)
-
-    # Replace emoji with text for the expression of the emoji
-    text = emoji.demojize(text)
 
     # Convert to lowercase
     text = text.lower()
@@ -157,84 +155,55 @@ def clean_tweet_data(text):
     return text
 
 
-election_results_data = read_data("../data/election_results/voting.csv")
+# Load the datasets
 trump_tweets = read_data("../data/tweets/hashtag_donaldtrump.csv")
 biden_tweets = read_data("../data/tweets/hashtag_joebiden.csv")
 
-
-# Remove irrelevant columns
+# Initialize a list of irrelevant columns
 irrelevant_cols = ['source', 'user_id', 'user_name', 'user_screen_name',
-                   'user_description', 'user_join_date', 'collected_at']
+                   'user_description', 'user_join_date', 'city',
+                   'collected_at']
 
+# Drop irrelevant columns from the datasets
 trump_tweets.drop(columns=irrelevant_cols, inplace=True)
 biden_tweets.drop(columns=irrelevant_cols, inplace=True)
 
-# Remove rows with missing values
-# Maybe reconsider only dropping if there is no state data available
-trump_tweets.dropna(axis=0, inplace=True)
+# Replace United States with United States of America
+trump_tweets['country'] = trump_tweets['country'].replace(
+    'United States', 'United States of America')
+biden_tweets['country'] = biden_tweets['country'].replace(
+    'United States', 'United States of America')
+
+# Filter out tweets that are not from the United States of America
+trump_tweets = trump_tweets[trump_tweets['country']
+                            == 'United States of America'].copy()
+biden_tweets = biden_tweets[biden_tweets['country']
+                            == 'United States of America'].copy()
+
+# Drop all rows with missing values
 biden_tweets.dropna(axis=0, inplace=True)
-
-pd.set_option('display.max_columns', None)
-
-# Filter tweets from the USA
-trump_usa_tweets = trump_tweets[trump_tweets['country'] == 'United States of America'].copy()
-biden_usa_tweets = biden_tweets[biden_tweets['country'] == 'United States of America'].copy()
-
-print(trump_usa_tweets.shape)
-print(biden_usa_tweets.shape)
-
-def get_language(text):
-    try:
-        return detect(text)
-    except:
-        return 'unknown'
-
+trump_tweets.dropna(axis=0, inplace=True)
 
 # Remove tweets that are in both datasets
-tids = trump_usa_tweets['tweet_id']
-bids = biden_usa_tweets['tweet_id']
-
-ids_tweets_in_common = set(trump_usa_tweets['tweet_id']).intersection(set(biden_usa_tweets['tweet_id']))
-
-trump_usa_tweets = trump_usa_tweets[~tids.isin(ids_tweets_in_common)]
-biden_usa_tweets = biden_usa_tweets[~bids.isin(ids_tweets_in_common)]
-
-print(trump_usa_tweets.shape)
-print(biden_usa_tweets.shape)
-
-# Filter out tweets that are not in English
-# trump_usa_tweets['language'] = trump_usa_tweets['tweet'].apply(lambda x: get_language(x))
-# biden_usa_tweets['language'] = biden_usa_tweets['tweet'].apply(lambda x: get_language(x))
-
-# trump_usa_tweets = trump_usa_tweets[trump_usa_tweets['language'] == 'en'].copy()
-# biden_usa_tweets = biden_usa_tweets[biden_usa_tweets['language'] == 'en'].copy()
-
-# print(trump_usa_tweets.shape)
-# print(biden_usa_tweets.shape)
+tids = trump_tweets['tweet_id']
+bids = biden_tweets['tweet_id']
+ids_tweets_in_common = set(trump_tweets['tweet_id']).intersection(
+    set(biden_tweets['tweet_id']))
+trump_tweets = trump_tweets[~tids.isin(ids_tweets_in_common)]
+biden_tweets = biden_tweets[~bids.isin(ids_tweets_in_common)]
 
 # Clean the tweets
-trump_usa_tweets['tweet'] = trump_usa_tweets['tweet'].apply(clean_tweet_data)
-biden_usa_tweets['tweet'] = biden_usa_tweets['tweet'].apply(clean_tweet_data)
+trump_tweets['tweet'] = trump_tweets['tweet'].apply(clean_tweet_data)
+biden_tweets['tweet'] = biden_tweets['tweet'].apply(clean_tweet_data)
 
-# Sentiment analysis
-trump_usa_tweets['sentiment_polarity'] = trump_usa_tweets['tweet'].apply(
+# Sentiment analysis of the tweets
+trump_tweets['sentiment_polarity'] = trump_tweets['tweet'].apply(
     lambda x: TextBlob(x).sentiment.polarity)
-biden_usa_tweets['sentiment_polarity'] = biden_usa_tweets['tweet'].apply(
+biden_tweets['sentiment_polarity'] = biden_tweets['tweet'].apply(
     lambda x: TextBlob(x).sentiment.polarity)
-
-trump_usa_tweets['sentiment_subjectivity'] = trump_usa_tweets['tweet'].apply(
-    lambda x: TextBlob(x).sentiment.subjectivity)
-biden_usa_tweets['sentiment_subjectivity'] = biden_usa_tweets['tweet'].apply(
-    lambda x: TextBlob(x).sentiment.subjectivity)
-
-pd.set_option('display.max_columns', None)
-print(trump_usa_tweets['tweet'].head())
-print(biden_usa_tweets['tweet'].head())
-print(trump_usa_tweets['sentiment_polarity'].head())
-print(biden_usa_tweets['sentiment_polarity'].head())
-print(trump_usa_tweets['sentiment_subjectivity'].head())
-print(biden_usa_tweets['sentiment_subjectivity'].head())
 
 # Save the new datasets to csv
-trump_usa_tweets.to_csv('../data/tweets/cleaned_hashtag_donaldtrump.csv', index=False)
-biden_usa_tweets.to_csv('../data/tweets/cleaned_hashtag_joebiden.csv', index=False)
+trump_tweets.to_csv(
+    '../data/tweets/cleaned_hashtag_donaldtrump.csv', index=False)
+biden_tweets.to_csv(
+    '../data/tweets/cleaned_hashtag_joebiden.csv', index=False)
